413b45e2b93ed77f247a1983f78c825cd1dde69d
hybae430 <violet9643@naver.com>
2021-01-06 23:44:18 +0900

    0106

--- kotlintest/MainActivity.kt
+++ kotlintest/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlintest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
 // import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,17 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        button.setOnClickListener(object: View.OnClickListener {
-//            override fun onClick(p0: View?) {
-//                // to do...
-//            }
-//        })
+        button.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                // to do...
+            }
+        })
 
         // 람다식으로 바꾸기 위한 조건
         // 1. Kotlin interface가 아닌 자바 인터페이스여야 합니다.
         // 2. 그 인터페이스는 딱 하나의 메소드만 가져야 합니다.
-//        button.setOnClickListener {
-//            // to do..
-//        }
+        button.setOnClickListener {
+            // to do..
+        }
     }
 }
